e01e177631709f8e2ff5e824b489cba78920f1b2
Nguyễn Trọng Toàn <toannt.181@gmail.com>
2017-08-29 16:10:27 +0700

    init

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
-require('dotenv').config({path: '.env'});
 
 /**
  * Create Express server.
